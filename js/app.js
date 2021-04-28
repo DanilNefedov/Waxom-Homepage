119b7abd3408b3b83eb7620e2590c355f093d744
DanilNefedov <ndanil2@ukr.net>
2021-04-28 10:47:56 +0300

    hw20

---
+++ js/app.js
@@ -0,0 +1,28 @@
+
+
+
+
+
+$(document).ready(function() {
+	
+	const swiper = new Swiper('.main-slider', {
+	  // Optional parameters
+	  loop: true,
+	  autoplay:{
+	  	delay:3000,
+	  },
+	  // If we need pagination
+	  pagination: {
+	    el: '.swiper-pagination',
+	  },
+
+	  // Navigation arrows
+	  navigation: {
+	    nextEl: '.slider__prev',
+	    prevEl: '.slider__next',
+	  },
+	 
+	});
+
+
+});
